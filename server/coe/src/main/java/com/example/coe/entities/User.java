cc765eee66de4385bc4187e7e5f190d62067f4f8
Erin Hanafin <erin.hanafin@hbr.org>
2023-07-25 14:04:26 +0100

    Created User Entity and User Repository

---
+++ server/coe/src/main/java/com/example/coe/entities/User.java
@@ -0,0 +1,28 @@
+package com.example.coe.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @SequenceGenerator(name = "users_sequence", sequenceName = "users_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_sequence")
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+    @Column(name = "email_address", nullable = false)
+    private String emailAddress;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
