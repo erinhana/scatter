06e7b0604507b504a0bfc512bdc6b3394d3c5e22
Erin Hanafin <erin.hanafin@hbr.org>
2023-09-05 15:15:12 +0100

    Created Activity Repo and Entity

---
+++ server/coe/src/main/java/com/example/coe/entities/Activity.java
@@ -0,0 +1,40 @@
+package com.example.coe.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Activity {
+    @Id
+    @SequenceGenerator(name = "activities_sequence", sequenceName = "activities_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "activities_sequence")
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
+
+
+
+//    todo_id INTEGER NOT NULL,
+//    time_spent INTEGER NOT NULL,
+//    CONSTRAINT activity_todos_todo_id_fk FOREIGN KEY (todo_id)
+//    REFERENCES todos (id)
---
+++ server/coe/src/main/java/com/example/coe/repositories/ActivityRepository.java
@@ -0,0 +1,10 @@
+package com.example.coe.repositories;
+
+import com.example.coe.entities.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
+}
+
