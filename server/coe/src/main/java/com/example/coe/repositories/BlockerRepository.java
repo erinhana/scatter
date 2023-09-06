1d1c10370655da9c6496608f9fafb462853ac5e4
Erin Hanafin <erin.hanafin@hbr.org>
2023-09-06 14:29:10 +0100

    Created Blocker Repo and Entity

---
+++ server/coe/src/main/java/com/example/coe/repositories/BlockerRepository.java
@@ -0,0 +1,9 @@
+package com.example.coe.repositories;
+
+import com.example.coe.entities.Blocker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlockerRepository extends JpaRepository<Blocker, Integer> {
+}
