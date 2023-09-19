6eb6c5a33d340153a5d2a63ec5c1850f89401c31
Erin Hanafin <erin.hanafin@hbr.org>
2023-09-19 12:51:43 +0200

    Updated Blocker Controller

--- server/coe/src/main/java/com/example/coe/controllers/BlockerController.java
+++ server/coe/src/main/java/com/example/coe/controllers/BlockerController.java
@@ -1,9 +1,18 @@
 package com.example.coe.controllers;
 
-import com.example.coe.models.blockers.*;
+import com.example.coe.entities.Blocker;
+import com.example.coe.exception.NotFoundException;
+import com.example.coe.models.blockers.BlockerViewModel;
+import com.example.coe.models.blockers.BlockerDetailViewModel;
+import com.example.coe.models.blockers.BlockerTypeViewModel;
+import com.example.coe.models.blockers.CreateBlockerViewModel;
+import com.example.coe.models.blockers.UpdateBlockerViewModel;
+import com.example.coe.repositories.BlockerRepository;
+import com.example.coe.utils.mapper.Mapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,20 +23,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/blockers")
 @Tag(name = "Blockers")
+@RequiredArgsConstructor
 public class BlockerController {
+
+    private final BlockerRepository blockerRepository;
+    private final Mapper mapper;
+
+
     @GetMapping
     @Operation(summary = "Get All Blockers")
     public ResponseEntity<List<BlockerViewModel>> getAllBlockers() {
 
-        var blockers= List.of(new BlockerViewModel(1, 1, "test blocker", "test blocker description", 1));
-        return null;
+
+        var blockers = blockerRepository.findAll();
+
+
+        return ResponseEntity.ok( mapper.map(blockers, BlockerViewModel.class));
     }
+
+
 
     @GetMapping(value = "/{blockerId}")
     @Operation(summary = "Get Blocker")
-    public ResponseEntity<BlockerDetailViewModel> getBlocker(@PathVariable int blockerId) {
-        return ResponseEntity.ok(new BlockerDetailViewModel(blockerId, 1, "Hoover broken", "Unable to clean room as hoover is broken", LocalDateTime.now(), LocalDateTime.now(), 1));
+    public ResponseEntity<BlockerDetailViewModel> getBlocker(@PathVariable int blockerId) throws Exception {
+        var blocker = blockerRepository.findById(blockerId).orElseThrow(() -> new NotFoundException("Blocker not found"));
+
+
+        return ResponseEntity.ok( mapper.map(blocker, BlockerDetailViewModel.class));
+
+
     }
+
+
 
     @GetMapping(value = "/types")
     @Operation(summary = "Get Blocker Types")
@@ -38,18 +65,39 @@ public class BlockerController {
     @PostMapping
     @Operation(summary = "Create Blocker")
     public ResponseEntity<BlockerViewModel> createBlocker(@RequestBody @Valid CreateBlockerViewModel model) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(new BlockerViewModel(1, 1, model.getTitle(), model.getDescription, 1));
+
+        var newBlocker = mapper.map(model, Blocker.class);
+        var createdBlocker = blockerRepository.save(newBlocker);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(createdBlocker, BlockerViewModel.class));
     }
+
+
 
     @PutMapping(value = "/{blockerId}")
     @Operation(summary = "Update Blocker")
-    public ResponseEntity<BlockerViewModel> updateBlocker(@PathVariable @Valid UpdateBlockerViewModel model) {
-        return ResponseEntity.ok(new BlockerViewModel(2, 2, model.getTitle(), model.getDescription(), 2));
+    public ResponseEntity<BlockerViewModel> updateBlocker(@PathVariable int blockerId, @RequestBody @Valid UpdateBlockerViewModel model) {
+
+        var existingBlocker = blockerRepository.findById(blockerId).orElseThrow(() -> new NotFoundException("Blocker not found"));
+
+        mapper.map(model, existingBlocker);
+        blockerRepository.save(existingBlocker);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
     }
+
+
 
     @DeleteMapping(value = "/{blockerId}")
     @Operation(summary = "Delete Blocker")
     public ResponseEntity<Void> deleteBlocker(@PathVariable int blockerId) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+       var existingBlocker = blockerRepository.findById(blockerId)
+               .orElseThrow(() -> new NotFoundException("No blocker exists with Id", blockerId));
+
+               blockerRepository.delete(existingBlocker);
+
+               return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+
 }
