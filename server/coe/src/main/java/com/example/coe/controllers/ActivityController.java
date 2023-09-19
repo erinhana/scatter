a42e5452956f4ef4736ed84915dbdd5f2a98921a
Erin Hanafin <erin.hanafin@hbr.org>
2023-09-19 14:59:01 +0200

    Updated ActivityController

--- server/coe/src/main/java/com/example/coe/controllers/ActivityController.java
+++ server/coe/src/main/java/com/example/coe/controllers/ActivityController.java
@@ -1,10 +1,21 @@
 package com.example.coe.controllers;
 
+import com.example.coe.entities.Activity;
+import com.example.coe.exception.NotFoundException;
 import com.example.coe.models.activities.*;
+import com.example.coe.models.activities.ActivityDetailViewModel;
+import com.example.coe.models.activities.ActivityTypeViewModel;
+import com.example.coe.models.activities.ActivityViewModel;
+import com.example.coe.models.activities.CreateActivityViewModel;
+import com.example.coe.models.activities.UpdateActivityViewModel;
 import com.example.coe.models.todos.TodoViewModel;
+import com.example.coe.models.users.UpdateUserViewModel;
+import com.example.coe.repositories.ActivityRepository;
+import com.example.coe.utils.mapper.Mapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,20 +27,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/activities")
 @Tag(name = "Activities")
+@RequiredArgsConstructor
+
+
 public class ActivityController {
+
+    private final ActivityRepository activityRepository;
+    private final Mapper mapper;
+
     @GetMapping
     @Operation(summary = "Get All Activities")
     public ResponseEntity<List<ActivityViewModel>> getAllActivities() {
 
-        var activities= List.of(new ActivityViewModel(1, 1, "test activity", "test activity description"));
-        return null;
+        var activities = activityRepository.findAll();
+        return ResponseEntity.ok(mapper.map(activities, ActivityViewModel.class));
     }
+
+
 
     @GetMapping(value = "/{activityId}")
     @Operation(summary = "Get Activity")
     public ResponseEntity<ActivityDetailViewModel> getActivity(@PathVariable int activityId) {
-        return ResponseEntity.ok(new ActivityDetailViewModel(activityId, 1, "Clean room", "Activity description", LocalDateTime.now(), LocalDateTime.now(), 15));
+
+        var activity = activityRepository.findById(activityId)
+                .orElseThrow(() -> new NotFoundException("No activity exists with this id"));
+
+        return ResponseEntity.ok(mapper.map(activity, ActivityDetailViewModel.class));
+
+
     }
+
+
+
 
     @GetMapping(value = "/types")
     @Operation(summary = "Get Activity Type")
@@ -37,23 +66,51 @@ public class ActivityController {
         return ResponseEntity.ok(new ActivityTypeViewModel());
     }
 
+
+
+
     @PostMapping
     @Operation(summary = "Create Activity")
     public ResponseEntity<ActivityViewModel> createActivity(@RequestBody @Valid CreateActivityViewModel model) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ActivityViewModel(1, 1, model.getTitle(), model.getDescription()));
+        var newActivity = mapper.map(model, Activity.class);
+        var createdActivity = activityRepository.save(newActivity);
+
+       return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(createdActivity, ActivityViewModel.class));
 
     }
+
+
+
+
 
     @PutMapping(value = "/{activityId}")
     @Operation(summary = "Update Activity")
-    public ResponseEntity<ActivityViewModel> updateActivity(@PathVariable @Valid UpdateActivityViewModel model) {
-        return ResponseEntity.ok(new ActivityViewModel(3, 3, model.getTitle(), model.getDescription()));
+    public ResponseEntity<ActivityViewModel> updateActivity(@PathVariable int activityId, @RequestBody @Valid UpdateActivityViewModel model) {
+        var activity = activityRepository.findById(activityId);
+        if (activity.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        mapper.map(model, activity.get());
+        activityRepository.save(activity.get());
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+
+
+
+
 
     @DeleteMapping(value = "/{activityId}")
     @Operation(summary = "Delete Activity")
     public ResponseEntity<Void> deleteActivity(@PathVariable int activityId) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        var existingActivity = activityRepository.findById(activityId)
+                .orElseThrow(() -> new NotFoundException("No activity exists with this Id", activityId));
+
+        activityRepository.delete(existingActivity);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
 
