5549b49258d5a11a2297536aa8470670f670c4c5
Erin Hanafin <erin.hanafin@hbr.org>
2023-10-31 12:05:25 +0000

    Added Unit Tests to BlockerControllerTest

--- server/coe/src/test/java/com/example/coe/controllers/BlockerControllerTest.java
+++ server/coe/src/test/java/com/example/coe/controllers/BlockerControllerTest.java
@@ -4,6 +4,8 @@ import com.example.coe.entities.Blocker;
 import com.example.coe.exception.NotFoundException;
 import com.example.coe.models.blockers.BlockerDetailViewModel;
 import com.example.coe.models.blockers.BlockerViewModel;
+import com.example.coe.models.blockers.CreateBlockerViewModel;
+import com.example.coe.models.blockers.UpdateBlockerViewModel;
 import com.example.coe.repositories.BlockerRepository;
 import com.example.coe.repositories.BlockerTypeRepository;
 import com.example.coe.utils.mapper.Mapper;
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -55,6 +58,7 @@ public class BlockerControllerTest {
         verify(mapper, times(1)).map(blockers, BlockerViewModel.class);
 
         assertThat(result.getBody()).isEqualTo(blockerViewModels);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
@@ -75,6 +79,7 @@ public class BlockerControllerTest {
         verify(mapper, times(1)).map(blocker, BlockerDetailViewModel.class);
 
         assertThat(result.getBody()).isEqualTo(blockerDetailViewModel);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
@@ -89,4 +94,61 @@ public class BlockerControllerTest {
 
     }
 
+    @Test
+    void createBlocker_whenCalledWithValidDetails_addsBlockerToRepository() {
+
+        // Arrange
+        var createBlockerViewModel = new CreateBlockerViewModel();
+        var blocker = new Blocker();
+        var createdBlocker = new Blocker();
+        var blockerViewModel = new BlockerViewModel();
+
+        when(mapper.map(createBlockerViewModel, Blocker.class)).thenReturn(blocker);
+        when(blockerRepository.save(blocker)).thenReturn(createdBlocker);
+        when(mapper.map(createdBlocker, BlockerViewModel.class)).thenReturn(blockerViewModel);
+
+        // Act
+        var result = blockerController.createBlocker(createBlockerViewModel);
+
+        // Assert
+        verify(mapper, times(1)).map(createBlockerViewModel, Blocker.class);
+        verify(blockerRepository, times(1)).save(blocker);
+        verify(mapper, times(1)).map(createdBlocker, BlockerViewModel.class);
+
+        assertThat(result.getBody()).isEqualTo(blockerViewModel);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    void updateBlocker_whenCalledWithValidDetails_returnsNoContent() {
+
+        // Arrange
+        var updateBlockerViewModel = new UpdateBlockerViewModel();
+        var blocker = new Blocker();
+
+        when(blockerRepository.findById(1)).thenReturn(Optional.of(blocker));
+
+        // Act
+        var result = blockerController.updateBlocker(1, updateBlockerViewModel);
+
+        // Assert
+        verify(blockerRepository, times(1)).findById(1);
+        verify(mapper, times(1)).map(updateBlockerViewModel, blocker);
+        verify(blockerRepository, times(1)).save(blocker);
+
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    void updateBlocker_whenCalledWithInValidDetails_throwsNotFoundException() {
+
+        // Arrange
+        var updateBlockerViewModel = new UpdateBlockerViewModel();
+
+        when(blockerRepository.findById(any())).thenReturn(Optional.empty());
+
+        // Act & Assert
+        ThrowableAssert.ThrowingCallable throwingCallable = () -> blockerController.updateBlocker(1, updateBlockerViewModel);
+        assertThatThrownBy(throwingCallable).isInstanceOf(NotFoundException.class).hasMessageContaining("Blocker not found");
+    }
 }
