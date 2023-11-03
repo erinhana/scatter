0737107bac10de01723f231d2ea89ccb292873f5
Erin Hanafin <erin.hanafin@hbr.org>
2023-11-03 14:36:56 +0000

    CreateBlockerViewModel- Integration test

--- server/coe/src/main/java/com/example/coe/models/blockers/CreateBlockerViewModel.java
+++ server/coe/src/main/java/com/example/coe/models/blockers/CreateBlockerViewModel.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateBlockerViewModel {
-    public String getDescription;
     @NotBlank
     private int userId;
     private String title;
---
+++ server/coe/src/test/java/com/example/coe/controllers/integration/BlockerControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.example.coe.controllers.integration;
+
+import com.example.coe.models.blockers.CreateBlockerViewModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BlockerControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void createBlocker_whenSuppliedWithValidData_returnsCreatedResponse() throws Exception {
+
+        var newBlocker = new CreateBlockerViewModel(1, "Test blocker", "Test blocker description", 1);
+
+
+        var result = mockMvc.perform(post("/blockers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newBlocker))).andExpect(status().isCreated());
+    }
+}
