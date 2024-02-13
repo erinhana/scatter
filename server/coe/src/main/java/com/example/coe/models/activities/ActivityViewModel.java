ed1065a8c666da18cbf01d155a3725e661968128
Erin Hanafin <erin.hanafin@hbr.org>
2024-02-13 14:26:09 +0000

    Adding to ActivityControllerIntegrationTest

--- server/coe/src/main/java/com/example/coe/models/activities/ActivityViewModel.java
+++ server/coe/src/main/java/com/example/coe/models/activities/ActivityViewModel.java
@@ -1,5 +1,6 @@
 package com.example.coe.models.activities;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
