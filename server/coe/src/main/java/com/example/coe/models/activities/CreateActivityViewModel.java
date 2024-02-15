252cfb0337c5b1b832d7c5b17817b5f8fdaa7615
Erin Hanafin <erin.hanafin@hbr.org>
2024-02-15 16:35:31 +0000

    Integration tests

--- server/coe/src/main/java/com/example/coe/models/activities/CreateActivityViewModel.java
+++ server/coe/src/main/java/com/example/coe/models/activities/CreateActivityViewModel.java
@@ -1,5 +1,6 @@
 package com.example.coe.models.activities;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateActivityViewModel {
-
+    
     private int todoId;
     private String title;
     private String description;
