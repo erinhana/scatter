4ff296ffb2b4a65e5fbd41a31ce4a6144aedee85
Erin Hanafin <erin.hanafin@hbr.org>
2024-02-21 16:54:13 +0000

    Activity Blocker changes

---
+++ server/coe/src/main/java/com/example/coe/entities/ActivityBlocker.java
@@ -0,0 +1,27 @@
+package com.example.coe.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "activity_blockers")
+@Getter
+public class ActivityBlocker
+{
+    @Id
+    @SequenceGenerator(name = "activity_blockers_sequence", sequenceName = "activity_blockers_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blocker_id", updatable = false, nullable = false)
+    private Blocker blocker;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", updatable = false, nullable = false)
+    private Activity activity;
+
+    @Column(name = "time_spent", nullable = false)
+    private int timeSpent;
+}
---
+++ server/coe/src/main/java/com/example/coe/repositories/ActivityBlockerRepository.java
@@ -0,0 +1,18 @@
+package com.example.coe.repositories;
+
+
+import com.example.coe.entities.ActivityBlocker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityBlockerRepository extends JpaRepository<ActivityBlocker, Integer> {
+    @Query("SELECT ab FROM ActivityBlocker ab " +
+            "JOIN Activity a ON ab.activity.id = a.id " +
+            "JOIN Todo t ON a.todo.id = t.id " +
+            "WHERE t.user.id = :userId")
+    List<ActivityBlocker> findActivityBlockersByUserId(int userId);
+}
