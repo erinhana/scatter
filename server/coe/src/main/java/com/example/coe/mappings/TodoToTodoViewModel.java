db01dadb961c58f58175877fd217319638b2bb70
Erin Hanafin <erin.hanafin@hbr.org>
2024-03-04 14:58:03 +0000

    Fix for TodoControllerIntegrationTest

--- server/coe/src/main/java/com/example/coe/mappings/TodoToTodoViewModel.java
+++ server/coe/src/main/java/com/example/coe/mappings/TodoToTodoViewModel.java
@@ -3,15 +3,16 @@ package com.example.coe.mappings;
 import com.example.coe.entities.Activity;
 import com.example.coe.entities.Todo;
 import com.example.coe.models.activities.ActivityViewModel;
+import com.example.coe.models.todos.TodoDetailViewModel;
 import com.example.coe.models.todos.TodoViewModel;
 import com.example.coe.utils.mapper.SourceToTargetMapping;
 import org.modelmapper.PropertyMap;
 
-public class TodoToTodoViewModel implements SourceToTargetMapping<Todo, TodoViewModel>
+public class TodoToTodoViewModel implements SourceToTargetMapping<Todo, TodoDetailViewModel>
 {
 
     @Override
-    public PropertyMap<Todo, TodoViewModel> mapFromSourceToTarget()
+    public PropertyMap<Todo, TodoDetailViewModel> mapFromSourceToTarget()
     {
         return new PropertyMap<>() {
             protected void configure() {
